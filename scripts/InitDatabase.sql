c131214dcabd1fd93781dade8484f681bc2e893a
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2024-06-12 18:14:18 -0300

    fix dependency injection after design time

---
+++ scripts/InitDatabase.sql
@@ -0,0 +1,60 @@
+﻿IF OBJECT_ID(N'[__EFMigrationsHistory]') IS NULL
+BEGIN
+    CREATE TABLE [__EFMigrationsHistory] (
+        [MigrationId] nvarchar(150) NOT NULL,
+        [ProductVersion] nvarchar(32) NOT NULL,
+        CONSTRAINT [PK___EFMigrationsHistory] PRIMARY KEY ([MigrationId])
+    );
+END;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+IF SCHEMA_ID(N'catalog') IS NULL EXEC(N'CREATE SCHEMA [catalog];');
+GO
+
+CREATE TABLE [catalog].[Categories] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    [CreatedDate] datetime2 NOT NULL,
+    [LastUpdated] datetime2 NOT NULL,
+    [Active] bit NOT NULL,
+    CONSTRAINT [PK_Categories] PRIMARY KEY ([Id])
+);
+GO
+
+CREATE TABLE [catalog].[Products] (
+    [Id] uniqueidentifier NOT NULL,
+    [Name] nvarchar(max) NOT NULL,
+    [Price] decimal(18,2) NOT NULL,
+    [CategoryId] uniqueidentifier NOT NULL,
+    [Tags] nvarchar(max) NULL,
+    [CreatedDate] datetime2 NOT NULL,
+    [LastUpdated] datetime2 NOT NULL,
+    [Active] bit NOT NULL,
+    CONSTRAINT [PK_Products] PRIMARY KEY ([Id]),
+    CONSTRAINT [FK_Products_Categories_CategoryId] FOREIGN KEY ([CategoryId]) REFERENCES [catalog].[Categories] ([Id]) ON DELETE CASCADE
+);
+GO
+
+CREATE INDEX [IX_Products_CategoryId] ON [catalog].[Products] ([CategoryId]);
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240531154044_InitialMigration', N'8.0.5');
+GO
+
+COMMIT;
+GO
+
+BEGIN TRANSACTION;
+GO
+
+INSERT INTO [__EFMigrationsHistory] ([MigrationId], [ProductVersion])
+VALUES (N'20240612181256_CategoryId', N'8.0.5');
+GO
+
+COMMIT;
+GO
+
--- src/WebApi/Program.cs
+++ src/WebApi/Program.cs
@@ -4,6 +4,7 @@ var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddSwaggerGen();
 builder.Services.AddEndpointsApiExplorer();
+builder.Services.AddCatalogModule(builder.Configuration);
 
 var app = builder.Build();
 
@@ -12,8 +13,6 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
-
-RegisterCatalogModule.AddCatalogModule(builder.Services, app.Configuration);
 
 app.MapCatalogEndpoints();
 

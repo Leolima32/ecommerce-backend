f23bbf6cc727baf8494639fe492d40908b57e4b4
Leonardo Ferreira <leonardo.limaferreira@yahoo.com.br>
2024-06-18 13:23:26 -0300

    add id to product view model

--- src/Modules/Catalog/Catalog.Application/ViewModels/ProductViewModel.cs
+++ src/Modules/Catalog/Catalog.Application/ViewModels/ProductViewModel.cs
@@ -5,20 +5,22 @@ namespace Catalog.Application.ViewModels;
 
 public record ProductViewModel
 {
-    public ProductViewModel(string name, decimal price, Guid categoryId, string? tags = null)
+    private ProductViewModel(Guid id, string name, decimal price, Guid categoryId, string? tags = null)
     {
+        Id = id;
         Name = name;
         Price = price;
         CategoryId = categoryId;
-        Tags = !tags.IsNullOrEmpty() ? tags?.Split(",") : null;
+        Tags = !tags.IsNullOrEmpty() ? tags?.Split(",") : [];
     }
     
+    public Guid Id { get; set; }
     public string Name { get; set; } = string.Empty;
     public decimal Price { get; set; }
     public Guid CategoryId { get; set; }
     public IEnumerable<string>? Tags { get; set; }
 
     public static ProductViewModel FromProduct(Product product) {
-        return new ProductViewModel(product.Name, product.Price, product.CategoryId, product.Tags);
+        return new ProductViewModel(product.Id, product.Name, product.Price, product.CategoryId, product.Tags);
     }
 }
